8b8c090e113e5d1e833309b0a381883aea854877
VitorSpornraft <vitorspornraft@gmail.com>
2025-07-06 19:40:49 -0300

    ex1-1 ok

---
+++ exercicio-1_1/src/Calculadora.java
@@ -0,0 +1,25 @@
+public class Calculadora {
+
+    double n1;
+    double n2;
+
+    public double somar() {
+        return n1 + n2;
+    }
+
+    public double subtrair() {
+        return n1 - n2;
+    }
+
+    public double multiplicar() {
+        return n1 * n2;
+    }
+
+    public double dividir() {
+        if (n2 == 0) {
+            System.out.println("Não é possível dividir por 0!.");
+            return 0;
+        }
+        return n1 / n2;
+    }
+}
---
+++ exercicio-1_1/src/CalculadoraMain.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class CalculadoraMain {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        Calculadora calc = new Calculadora();
+
+        System.out.print("Digite o primeiro numero: ");
+        calc.n1 = sc.nextDouble();
+
+        System.out.print("Digite o segundo numero: ");
+        calc.n2 = sc.nextDouble();
+
+        System.out.print("Escolha a operação (+ - * /): ");
+        char op = sc.next().charAt(0);
+
+        double r;
+
+        if (op == '+'){
+            r = calc.somar();
+        }else if (op == '-') {
+            r = calc.subtrair();
+        } else if (op == '*') {
+            r = calc.multiplicar();
+        } else if (op == '/') {
+            r = calc.dividir();
+        } else {
+            System.out.println("Operação inválida.");
+            sc.close();
+            return;
+        }
+
+        System.out.printf("Resultado: %.2f%n", r);
+        sc.close();
+    }
+}
